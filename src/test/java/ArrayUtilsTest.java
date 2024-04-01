983fcfc18b7a76117da2d622e06215a95937f15e
Jeff <jcui19@jhu.edu>
2024-04-01 17:45:41 -0400

    add more tests to test count of

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,29 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void testCountOfNull() {
+        assertThrows(NullPointerException.class, () -> {ArrayUtils.countOf(null, 3);});
+    }
+
+    @Test
+    public void testCountOfEmpty() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{}, 3));
+    }
+
+    @Test
+    public void testCountOfNonExistent() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{1, 2}, 3));
+    }
+
+    @Test
+    public void testCountOfExists() {
+        assertEquals(2, ArrayUtils.countOf(new int[]{1, 2, 3, 1}, 1));
+    }
+
+    @Test
+    public void testCountOfAllSame() {
+        assertEquals(4, ArrayUtils.countOf(new int[]{1,1,1,1}, 1));
+    }
 }
